4ac8e2994efdd1b31b1969f0da6729ba92a360a3
CEM KAAN KÖSALI <cemkaan@gmail.com>
2019-02-03 09:26:48 +0300

    temel türkçeleştirme
    menüden blog kaldırdım

--- data/config.js
+++ data/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'İş Sitesi', // Site title.
+  siteTitleAlt: 'iş', // Alternative site title for SEO.
   siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
   siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
@@ -13,7 +13,7 @@ module.exports = {
   userTwitter: 'vaibhaved',
   userLocation: 'Delhi NCR, India',
   userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'CemKaanKösalı.com.tr © ', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
